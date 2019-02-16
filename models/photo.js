30940bfed43f8adadb023ea420ee8610f25b7dca
sagig <sagig@sagig7490-ubuntu18.checkpoint.com>
2019-02-16 13:19:34 +0200

    updates

--- models/photo.js
+++ models/photo.js
@@ -1,16 +1,30 @@
 var mongoose    =   require('mongoose');
 
 var photo = new mongoose.Schema({
-        photoID: String,
+        photoID: {
+            type: String,
+            required: true,
+            unique: true
+        },
         name: String,
-        URL: String,
-        userID: String,
+        URL: {
+            type: String,
+            required: true
+        },
+        userID: {
+            type: String,
+            required: true
+        },
         likes: Number,
         likes_array: [String], //include the users id
         num_of_rates: Number, // how many people rated
         rates_sum: Number,
         rates_array: [String], //include the users id
-        total_rate: Number   
+        total_rate: {
+            type: Number,
+            min: 0,
+            max: 5
+        }   
     });
 
 var Photo       =   mongoose.model('Photo', photo);
