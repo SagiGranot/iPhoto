a56f5857c98c92168428bb84ae3161cd47dae55e
sagig <sagig@sagig7490-ubuntu18.checkpoint.com>
2019-02-19 21:55:05 +0200

    merg

--- controllers/feedback.ctrl.js
+++ controllers/feedback.ctrl.js
@@ -84,7 +84,7 @@ module.exports = {
             res.json(result)
         })
         .catch(err => {
-            res.status(404).send(err)
+            res.status(404).send("Error:"+err)
         })
     }
 }
